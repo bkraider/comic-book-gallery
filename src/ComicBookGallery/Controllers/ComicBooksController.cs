cf71d4c78982fd5c261bcf0e8e4c71f682de921a
bhanu korremula <bkraider@gmail.com>
2017-07-15 23:46:14 -0400

    added comic books controller

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        // GET: ComicBooks
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Detail()
+        {
+            return "Hello from the comic books controller";
+        }
+    }
+}
